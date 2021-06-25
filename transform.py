83d07498a69224e04cb54eeac1b8e85e369736cf
Ravi Tamiri <ravi.tamiri@gmail.com>
2021-06-25 06:54:00 -0700

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,58 +1,164 @@
-from pyspark.sql import SparkSession
-from pyspark import SparkContext
-from pyspark.sql.functions import countDistinct
-from pyspark.sql.functions import col
+import re
+import os
+from pyspark import StorageLevel
+from pyspark import SparkContext,SparkConf
+from pyspark.sql import SQLContext, HiveContext, SparkSession, Window
+from pyspark.sql.functions import lag, col,lead,to_timestamp,split,udf
+from pyspark.sql.functions import StringType,count,regexp_replace,when,explode,trim
+from os import listdir
+import pandas as pd
+import email
 
-SparkContext.setSystemProperty('spark.executor.memory', '8g')
-sc = SparkContext("local", "Spark_Enron")
-spark = SparkSession \
-    .builder \
-    .appName("Spark_Enron") \
-    .getOrCreate()
-# loading
-df = spark.read.format("csv") \
-    .option("header", "true") \
-    .load("./export/*.csv")
-print(df.columns)
 
-Q1_df = df.filter(col("id").contains("JavaMail")).groupBy("id").agg(countDistinct("id"))
-Q1_file = open("Q1.txt", "w")
-Q1_file.write(str(Q1_df.count()))
-Q1_file.close()
-Q2_df = df.filter(
-    col("to").contains("@")) \
-    .groupBy('to') \
-    .agg(countDistinct("id")) \
-    .filter("to is NOT null AND to != ''") \
-    .sort(col("count(id)").desc())
-Q2_df.show()
-Q2_df.repartition(1) \
-    .write.format("com.databricks.spark.csv") \
-    .option("header", "true") \
-    .save("Q2.txt")
-empDF.alias("emp1").join(empDF.alias("emp2"), \
-                         col("emp1.superior_emp_id") == col("emp2.emp_id"), "inner") \
-    .select(col("emp1.emp_id"), col("emp1.name"), \
-            col("emp2.emp_id").alias("superior_emp_id"), \
-            col("emp2.name").alias("superior_emp_name")) \
-    .show(truncate=False)
-Q3_df = df.alias("df1").join(df.alias("df2"), col("df1.from") == col("df2.to") & col("df1.id") == col("df2.id")) \
-    .select(col("df1.from"), col("df1.to"), col(df1.
-"df1.subject"), col("df1.date")) \
-    .withColumn("time_for_reply", col("df2.date") - col("df1.date")) \
-    .agg(Distinct("id")) \
- \
-df.createOrReplaceTempView("q3_view")
-spark.sql(
-    "CREATE TEMPORARY VIEW q3_view_int AS SELECT first(m1.from) as from,first(m1.to) as to, first(m1.subject) as subject, first(m1.date) as date, min(m2.date) as response_date" +
-    " FROM q3_view m1 JOIN q3_view m2 ON m1.from = m2.to AND to_timestamp(m2.date) > to_timestamp(m1.date)" +
-    " GROUP BY m1.from, m1.to, m1.date")
+# stage = 'spark_init'
+# spark = SparkSession.builder.appName(stage).enableHiveSupport().getOrCreate()
+# sc = SparkContext("local", "stage App")
 
-spark.sql("SELECT * from q3_view_int").show()
+conf=SparkConf()
+conf.set("spark.executor.memory", "10g")
+conf.set("spark.driver.memory", "4g")
+conf.set("spark.cores.max", "6")
+# conf.set("spark.driver.extraClassPath",
+#     driver_home+'/jdbc/postgresql-9.4-1201-jdbc41.jar:'\
+#     +driver_home+'/jdbc/clickhouse-jdbc-0.1.52.jar:'\
+#     +driver_home+'/mongo/mongo-spark-connector_2.11-2.2.3.jar:'\
+#     +driver_home+'/mongo/mongo-java-driver-3.8.0.jar') 
 
-spark.sql(
-    "CREATE TEMPORARY VIEW q3_view_int2 AS SELECT from, to , subject, date, to_timestamp(response_date) - to_timestamp(date) as time_for_reply" +
-    " from q3_view_int")
+sc = SparkContext.getOrCreate(conf)
 
-spark.sql(" SELECT from, to , subject, date,time_for_reply " +
-          " from q3_view_int where time_for_reply is NOT NULL ORDER BY time_for_reply asc").show()
+spark = SQLContext(sc)
+
+DIR = "/Users/ravitamiri/Downloads/maildir/"
+# Read all the folders and subfolders in the data
+loc = listdir(DIR)
+
+for index,l in enumerate(loc):
+      li = []
+      print(l)
+      for r, d, files in os.walk(DIR+ l+ "/"):
+            if r[-1] != "/":
+                  li.append(r +"/*")
+      data_dir= sc.wholeTextFiles(",".join(li))
+      data_dir = data_dir.toDF()
+      data_dir = data_dir.dropDuplicates()
+      if index == 0:
+            data= data_dir
+      else:
+            data = data.union(data_dir)
+
+# data = spark.read.format('parquet').options(headers = True).load('Downloads/enron')
+data = data.withColumnRenamed("_1", "location")
+data = data.withColumnRenamed("_2", "message")
+data = data.withColumn("user" ,split(col("location"),"/").getItem(5))
+data = data.withColumn("message" ,regexp_replace("message","N","_n"))
+
+
+ToEmailParses = udf(lambda z: ToEmailParse(z), StringType())
+spark.udf.register("ToEmailParse", ToEmailParses)
+
+def ToEmailParse(s):
+        msg = email.message_from_string(r'{}'.format(s))
+        return msg['to']
+data = data.withColumn('to',ToEmailParses('message'))
+
+
+FromEmailParses = udf(lambda z: FromEmailParse(z), StringType())
+spark.udf.register("FromEmailParse", FromEmailParses)
+
+def FromEmailParse(s):
+        msg = email.message_from_string(r'{}'.format(s))
+        return msg['from']
+data = data.withColumn('from',FromEmailParses('message'))
+
+
+DateParses = udf(lambda z: ToEmailParse(z), StringType())
+spark.udf.register("ToEmailParse", DateParses)
+
+def ToEmailParse(s):
+        msg = email.message_from_string(r'{}'.format(s))
+        return msg['Date']
+data = data.withColumn('Date',DateParses('message'))
+
+
+SubjectParses = udf(lambda z: SubjectParse(z), StringType())
+spark.udf.register("SubjectParse", SubjectParses)
+
+def SubjectParse(s):
+        msg = email.message_from_string(r'{}'.format(s))
+        return msg['Subject']
+
+data = data.withColumn('subject',SubjectParses('message'))
+
+DateCorParses = udf(lambda z: DateCorParse(z), StringType())
+spark.udf.register("DateCorParse", DateCorParses)
+
+def DateCorParse(s):
+       return "-".join(s.split(" ")[1:5])
+
+
+data = data.withColumn('DateCor',DateCorParses('Date'))
+data = data.withColumn('DateCor',to_timestamp("DateCor", "dd-MMM-yyyy-HH:mm:ss"))
+
+data = data.withColumn("subject", regexp_replace("subject","\s+",""))
+# data = data.withColumn("subject", regexp_replace("subject"," ",""))
+
+
+# Question 1 Answer
+# Get the count of the emails
+data.select("location").distinct().count()
+
+text_file = open("Q1.txt", "w")
+text_file.write("%s" % int(data.select("location").distinct().count()))
+text_file.close()
+
+# Verified by calculating the number of files from os directory
+
+data = data.select("*",(explode(split(col("to"), ",")).alias("to_clean"))).where('to_clean != ""')
+data = data.withColumn("to_clean", regexp_replace("to_clean","\s+",""))
+data = data.withColumn("to_clean", regexp_replace("to_clean"," ",""))
+
+
+
+
+
+# Question 2
+# Here we explode to to column as it is a comma separated list and then perform 
+data_q2 = data.groupBy('to_clean').agg(count("message").alias("total_messages")).orderBy("total_messages",ascending=False)
+
+data_q2 = data_q2.toPandas()
+data_q2.to_csv("Q2.txt",index = False,sep = "\t")
+# Verfied by looking at the volume of message for a person they seem to be inline 
+
+# Question 3:
+# Here we do a self join and try to match the from and to variable and then match the subject with "Re:" Removed to denote response
+# Question is asked about millliseconds be the date is only time Seconds
+data_join = data.select(['to','from','DateCor','subject','message'])
+data_join = data_join.withColumnRenamed("to", "to_self")
+data_join = data_join.withColumnRenamed("DateCor", "DateCor_self")
+data_join = data_join.withColumnRenamed("message", "message_self")
+data_join = data_join.withColumnRenamed("subject", "subject_self")
+data_join = data_join.withColumnRenamed("from", "from_self")
+data_join = data_join.where(col("to_self") != col("from_self"))
+data_join = data_join.where(col("subject_self") != "")
+data = data.withColumnRenamed("from", "from_")
+
+data_join_df  = data.join(data_join, (data.from_ == data_join.to_self) & (data.to == data_join.from_self))
+data_join_df.persist(StorageLevel.MEMORY_AND_DISK_2)
+data_join_df.count()
+diff_secs_col = col("DateCor_self").cast("long")-col("DateCor").cast("long") 
+data_join_df = data_join_df.withColumn( "diff_secs", diff_secs_col )
+data_join_df = data_join_df.withColumn( "subject_self", regexp_replace("subject_self","Re:","") )
+data_join_df = data_join_df.where(col("subject") == col("subject_self"))
+data_join_df = data_join_df.where(col("diff_secs") >= data_join_df.agg({"diff_secs": "min"}).collect()[0])
+
+mail = data_join_df.where(col("diff_secs") == 0).collect()[0]
+msg = email.message_from_string(r'{}'.format(mail['message']))
+
+text_file = open("Q3.txt", "w")
+text_file.write("%s\n" % msg['from'])
+text_file.write("%s\n" % msg['to'])
+text_file.write("%s\n" % msg['Subject'])
+text_file.write("%s\n" % msg['Date'])
+text_file.write("%s" % mail['diff_secs'])
+text_file.close()
+
